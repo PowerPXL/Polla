894c72e4c6fad6521904002732da37ac271a4ab8
PP <pavlos.poli@gmail.com>
2025-06-18 14:06:04 +0200

    tb oauth

--- app.py
+++ app.py
@@ -39,18 +39,14 @@ login_manager = LoginManager(app)
 login_manager.login_view = "login"
 
 # --- OAuth setup ---
-oauth = OAuth(app)
-
 google = oauth.register(
     name='google',
     client_id=os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url='https://oauth2.googleapis.com/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://www.googleapis.com/oauth2/v1/userinfo',
-    client_kwargs={'scope': 'openid email profile'}
-    # REMOVE redirect_uri here!
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
 )
 
 # --- User klass och in-memory storage ---
